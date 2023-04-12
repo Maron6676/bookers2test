30936263fd8d6ce582ad2d381dba94710f0914f4
Maron6676 <127210557+Maron6676@users.noreply.github.com>
2023-04-12 04:46:04 +0000

    [add]Bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+
+const webpack = require("webpack")
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquey',
+    jQuery: 'jquery/src/jquey',
+    Popper: 'popper.js'
+  })
+)
