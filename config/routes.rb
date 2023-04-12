0bf55060ed30f90711c6a84316f877fff46ec902
Maron6676 <127210557+Maron6676@users.noreply.github.com>
2023-04-12 04:30:41 +0000

    [add]アソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/about'
+  root to: 'homes#top'
+  resource :users, only:[:index, :show, :edit]
+  resource :books, only:[:index, :show, :edit]
+  get 'home/about' =>'homes#about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
